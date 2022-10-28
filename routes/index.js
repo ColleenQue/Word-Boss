c32575865d191d9a44c45556d258636a76805e79
Dylan Tran <dmtran20@gmail.com>
2022-10-28 18:58:12 -0400

    Updated Sprint2

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const loginRoute = require('./login');
 const signupRoute=require('./signup');
 const homeRoutes=require('./home');
+const spellingRoute=require('./spelling')
 
 const constructorMethod = (app) => {
   app.use('/login', loginRoute);
   app.use('/signup',signupRoute);
   app.use('/home',homeRoutes);
+  app.use('/spelling', spellingRoute);
   app.use('*', (req, res) => {
     res.redirect('/home');
   });
