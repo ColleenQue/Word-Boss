33333b62a97c9b5895f3889462d54ef43f6a9cd3
Jae <84739325+jaejang02@users.noreply.github.com>
2022-10-28 20:46:29 -0400

    Update multiplce_choice_def.js
    This is not finished back-end of multiple choice.

--- multiplce_choice_def.js
+++ multiplce_choice_def.js
@@ -1,8 +1,8 @@
 import words from './words.json ' assert{type: 'json'};
 
+let list_of_questions = []
 
-
-
+// creates the list of questions;
 for(let key in words){
     let wrong1 = Math.floor(Math.random()*words.length);
     let wrong2 = Math.floor(Math.random()*words.length);
@@ -13,5 +13,15 @@ for(let key in words){
         wrong3 = Math.floor(Math.random()*word.length);
     }
 
-    
+    let question = "What word is defined as: " + words[key].defintion;
+    let choices = [key[wrong1].word, key[wrong3].word, answer, key[wrong2].word];
+    let answer = words[key].word;
+
+    let current_object = {question: question, choices: choices, answer: answer};
+    list_of_questions.push(current_object);
+
 }
+
+// for(var i = 0; i < list_of_questions.length; i++){
+//     var
+// }
