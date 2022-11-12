77c6283509ca0fca51c5cc2a064516de916249a0
Jae <84739325+jaejang02@users.noreply.github.com>
2022-11-12 00:11:32 -0500

    Completed multi quiz
    Just need to do the css for the correct and incorrect html.

--- routes/quiz.js
+++ routes/quiz.js
@@ -3,42 +3,28 @@ const quiz = require('../data/quiz');
 const express = require('express');
 const router = express.Router();
 
-
+let question;
 router.get('/', async (req, res) => {
-    let question = await quiz.generateQuestion();
+    question = await quiz.generateQuestion();
     res.render('pages/quiz', {
-        definition : question[0],
-        choice1 : question[1][0],
-        choice2 : question[1][1],
-        choice3 : question[1][2],
-        choice4 : question[1][3]
+        definition : question[1],
+        choice1 : question[2][0],
+        choice2 : question[2][1],
+        choice3 : question[2][2],
+        choice4 : question[2][3]
     });
 
 })
-router.post('', async (req, res) => {
-
-
-    let answer = req.body.word;
-    let correct;
-    let incorrect;
-
-    try {
-        var obj = await vocab.WordToday();
-        obj = obj.word;
-        word = obj.word;
-        correct = spelling.spellCheck(answer,word);
-        if(correct==false){
-            incorrect = true;
-        }
-        res.render('pages/spelling', { type: type, definition:definition, word:word, correct:correct, incorrect:incorrect});
-        return;
-
-      }
-      catch (e) {
-        res.status(400).render('pages/spelling', { type: type, definition:definition, word:word, correct:correct,error:e});
-        return;
-      }
-
+router.post('/', async (req, res) => {
+    let search = req.body;
+    search = search.choice;
+    let correct = question[0];
+    if(search == correct){
+        return res.render('pages/correct');
+    }
+    else{
+        return res.render('pages/incorrect')
+    }
 })
 
 module.exports = router;
