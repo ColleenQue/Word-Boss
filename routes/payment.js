4cea75e31ac1f0f14eeb58101b1c3749e598850a
Dylan Tran <dmtran20@gmail.com>
2022-11-14 13:41:41 -0500

    Payment Portal basics
    Route, style, validation, and handlebar page

--- routes/payment.js
+++ routes/payment.js
@@ -1,29 +1,50 @@
 const payment = require('../data/payment');
 const express = require('express');
 const router = express.Router();
+const validation=require('../validation');
 
 
-router.get('/', async (req, res) => {
+router.get('', async (req, res) => {
 
-    var word,type,definition;
+  res.render('pages/payment', {});
+
+});
+
+router.post('/', async (req, res) => {
     
+  let cname = req.body.cname;
+  let cardnumber = req.body.cardnumber;
+  let cvc=req.body.cardnumberCVC;
+  let cardnumberExp=req.body.cardnumberExp;
+  let creditCardPostalCode=req.body.creditCardPostalCode;
+  let result;
+  try {
+    cname= validation.checkCname(req.body.cname);
+    cardnumber = validation.validateCreditCard(req.body.cardnumber);
+    cvc = validation.validateCreditCardCVC(req.body.cardnumberCVC);
+    cardnumberExp=validation.validateCreditCardExpirationDate(cardnumberExp);
+    creditCardPostalCode=validation.validateCreditCardPostalCode(req.body.creditCardPostalCode);
 
 
-    try {
-        var obj = await vocab.WordToday();
-        var wordToday =obj.word ;
-        type = wordToday.type;
-        definition = wordToday.definition;
-        word = wordToday.word;
 
-        res.render('pages/payment', { card: card, type: type, definition:definition});
-      }
-      catch (e) {
-        res.status(400).render('pages/payment', { word: word, type: type, definition:definition,error:e});
-        return;
-      }
+    result = await user.checkUser(username, password);
+  }
+  catch (e) {
+    res.status(400).render('pages/payment', { err: true, message: e ,title:"Log in"});
+    return;
+  }
 
 
-},)
+  if (result.authenticated === true) {
+    req.session.user=username;
+    //res.render('pages/home', {login:true});
+    res.redirect('/home');
+
+  }
+  else {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+
+})
 
 module.exports = router;
